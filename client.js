04f4b6f0fc1209a8a5eea95c18e68d234aa00b8b
gandnolphinnn <gandolfiluca03@gmail.com>
2022-05-03 15:00:26 +0200

    html and basci js

---
+++ client.js
@@ -0,0 +1,3 @@
+window.onload = function() {
+	// if get = ip address;
+}
--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 window.onload = function() {
-
+	let ip = document.querySelector('input');
+	if (ip.innerText == '123') { //fix
+		alert();
+	}
 }
