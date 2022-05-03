60bafee22be0a7653e4b878557889a3b129b659f
Gandolphinnn <gandolfiluca03@gmail.com>
2022-05-04 00:41:48 +0200

    js on inputs and node.js first test failed :(

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 window.onload = function() {
 	let ip = document.querySelector('input');
-	if (ip.innerText == '123') { //fix
-		alert();
+	ip.onkeyup = (e) => {
+		if ((e.code != 'Enter' && e.code != 'Backspace' && e.code.substring(0, 5) != 'Digit')
+		|| (e.code == 'Backspace' && ip.value.length % 4 == 3)) {
+			ip.value = ip.value.substring(0, ip.value.length-1);
+		}
+		if (ip.value.length % 4 == 3 && ip.value.length < 15) {
+			ip.value += '.';
+		}
+	}
+	document.querySelector('.left').onclick = () => {
+		if (ip.value.length == 15) {
+			window.location = '?conn=' + ip.value;
+		}
+		else {
+			ip.focus();
+		}
 	}
 }
